e9daa1ccbeb5817a497f4674f2aea434e4ffc779
Lucien Thomas <lthomas@sciport.org>
2024-07-03 17:14:09 -0500

    dev: temperature sensor now works, switched from embedded_dht to dht_sensor crate

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,10 @@ use rp_pico::hal::{
     timer::Timer,
 };
 
-use embedded_hal::delay::DelayNs;
+use embedded_hal::blocking::delay::DelayMs;
 
-use dht_embedded::{Dht22, DhtSensor, NoopInterruptControl};
+use dht_sensor::dht22;
+use dht_sensor::DhtReading;
 
 use hd44780_driver::{HD44780, DisplayMode, Cursor, CursorBlink, Display};
 
@@ -64,9 +65,7 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-
-    let mut shared_timer = SharedTimer::new(timer);
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
     // Set up the pins
     let pins = Pins::new(
@@ -79,7 +78,7 @@ fn main() -> ! {
     let sda_pin: Pin<Gpio4, FunctionI2C, PullUp> = pins.gpio4.into_pull_up_input().into_function();
     let scl_pin: Pin<Gpio5, FunctionI2C, PullUp> = pins.gpio5.into_pull_up_input().into_function();
 
-    let gpio0: InOutPin<_> = InOutPin::new(pins.gpio0.into_function::<FunctionSioOutput>());
+    let mut gpio0: InOutPin<_> = InOutPin::new(pins.gpio0.into_function::<FunctionSioOutput>());
 
     // Create I2C driver
     let i2c = I2C::i2c0(
@@ -91,52 +90,39 @@ fn main() -> ! {
         &clocks.peripheral_clock,
     );
 
-    let mut sensor = Dht22::new(NoopInterruptControl, &mut shared_timer, gpio0);
-
-    let mut lcd = HD44780::new_i2c(i2c, 0x3F, &mut shared_timer).unwrap();
+    // Initialize LCD with cloned timer
+    let mut timer_clone_for_lcd = timer.clone();
+    let mut lcd = HD44780::new_i2c(i2c, 0x3F, &mut timer_clone_for_lcd).unwrap();
 
     // Initialize the LCD
-    lcd.reset(&mut shared_timer).unwrap();
+    lcd.reset(&mut timer.clone()).unwrap();
 
     // Set display mode
     lcd.set_display_mode(DisplayMode {
         display: Display::On,
         cursor_visibility: Cursor::Invisible,
         cursor_blink: CursorBlink::Off,
-    }, &mut shared_timer).unwrap();
+    }, &mut timer.clone()).unwrap();
 
     // Clear the display
-    lcd.clear(&mut shared_timer).unwrap();
+    lcd.clear(&mut timer.clone()).unwrap();
 
     loop {
-       match sensor.read() {
-        Ok(reading) => {
-            let temp = reading.temperature();
-            let mut temp_str: String<32> = String::new();
-            write!(temp_str, "Temp: {:.1}C", temp).unwrap();
-            lcd.clear(&mut shared_timer).unwrap();
-            lcd.write_str(&temp_str, &mut shared_timer).unwrap();
+        match dht22::Reading::read(&mut timer.clone(), &mut gpio0)  {
+            Ok(dht22::Reading { temperature, relative_humidity: _, }) => {
+                let temp_celsius = temperature as f32;
+                let temp_fahrenheit = temp_celsius * 9.0 / 5.0 + 32.0;
+                let mut temp_str: String<32> = String::new();
+                write!(temp_str, "Temp: {:.1}F", temp_fahrenheit).unwrap();
+                
+                // Display the temperature on the LCD
+                lcd.clear(&mut timer.clone()).unwrap();
+                lcd.write_str(&temp_str, &mut timer.clone()).unwrap();
+            }
+            Err(_) => {
+                continue;
+            }
         }
-        Err(_) => {
-            continue;
-        }
-       }
-        shared_timer.delay_ms(2100);
-    }
-} 
-
-struct SharedTimer {
-    timer: Timer,
-}
-
-impl SharedTimer {
-    fn new(timer: Timer) -> Self {
-        SharedTimer { timer }
-    }
-}
-
-impl DelayNs for SharedTimer {
-    fn delay_ns(&mut self, ns: u32) {
-        self.timer.delay_ns(ns)
+        timer.clone().delay_ms(2100);
     }
 }
